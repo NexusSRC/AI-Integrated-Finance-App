e9a0951c0c609f7cd340d0a975475a9762587e98
Gnana Pragadeesh K <gnanapragadeesh12@gmail.com>
2024-10-04 22:47:47 +0530

    Adding of JS and CSS

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from langchain_community.llms import Ollama
 app = Flask(__name__)
 llm = Ollama(model="llama3")
 @app.route('/model/chat', methods=['POST'])
 def recommend(prompt):  # put application's code here
     response = llm.invoke(prompt, stop=['<|eot_id|>'])
-    return response
+    return jsonify({"response": response})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
