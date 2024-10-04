c1667fcb1626eb9c99442b33c9e11e9a1015f941
Gnana Pragadeesh K <gnanapragadeesh12@gmail.com>
2024-10-04 19:56:02 +0530

    baseline project

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from langchain_community.llms import Ollama
 app = Flask(__name__)
 llm = Ollama(model="llama3")
-@app.route('/')
+@app.route('/model/chat', methods=['POST'])
 def recommend(prompt):  # put application's code here
     response = llm.invoke(prompt, stop=['<|eot_id|>'])
     return response
