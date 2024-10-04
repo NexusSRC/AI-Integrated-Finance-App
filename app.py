9c678f2fd8809213d561b094a6be77cd7702f518
Gnana Pragadeesh K <gnanapragadeesh12@gmail.com>
2024-10-04 19:36:09 +0530

    baseline project

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from langchain_community.llms import Ollama
 app = Flask(__name__)
-
+llm = Ollama(model="llama3")
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def recommend(prompt):  # put application's code here
+    response = llm.invoke(prompt, stop=['<|eot_id|>'])
+    return response
 
 if __name__ == '__main__':
     app.run()
